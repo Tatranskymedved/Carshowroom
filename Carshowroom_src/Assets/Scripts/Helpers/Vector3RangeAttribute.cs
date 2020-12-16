525d1bafaece74a7563a6a0a4b4abd83f7f0f5b4
Jan Urbanec <UrbanecJ@seznam.cz>
2020-12-16 12:42:18 +0100

    Updated materials for lights and turned them on. Same for brakes/blinker.

--- Carshowroom_src/Assets/Scripts/Helpers/Vector3RangeAttribute.cs
+++ Carshowroom_src/Assets/Scripts/Helpers/Vector3RangeAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEditor;
 /* Copied from https://gist.github.com/DGoodayle/69c9c06eb0a277d833c5 , updated for Vector3
 
 Vector Range Attribute by Just a Pixel (Danny Goodayle @DGoodayle) - http://www.justapixel.co.uk
@@ -38,62 +37,5 @@ public class Vector3RangeAttribute : PropertyAttribute
         this.fMinZ = fMinZ;
         this.fMaxZ = fMaxZ;
         this.bClamp = bClamp;
-    }
-}
-
-[CustomPropertyDrawer(typeof(Vector3RangeAttribute))]
-public class Vector3RangeAttributeDrawer : PropertyDrawer
-{
-    const int helpHeight = 30;
-    const int textHeight = 16;
-    Vector3RangeAttribute rangeAttribute { get { return (Vector3RangeAttribute)attribute; } }
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        Color previous = GUI.color;
-        GUI.color = !IsValid(property) ? Color.red : Color.white;
-        Rect textFieldPosition = position;
-        textFieldPosition.width = position.width;
-        textFieldPosition.height = position.height;
-        EditorGUI.BeginChangeCheck();
-        Vector3 val = EditorGUI.Vector3Field(textFieldPosition, label, property.vector3Value);
-        if (EditorGUI.EndChangeCheck())
-        {
-            if (rangeAttribute.bClamp)
-            {
-                val.x = Mathf.Clamp(val.x, rangeAttribute.fMinX, rangeAttribute.fMaxX);
-                val.y = Mathf.Clamp(val.y, rangeAttribute.fMinY, rangeAttribute.fMaxY);
-                val.z = Mathf.Clamp(val.z, rangeAttribute.fMinZ, rangeAttribute.fMaxZ);
-            }
-            property.vector3Value = val;
-        }
-        Rect helpPosition = position;
-        helpPosition.y += 16;
-        helpPosition.height = 16;
-        DrawHelpBox(helpPosition, property);
-        GUI.color = previous;
-    }
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        if (!IsValid(property))
-        {
-            return 32;
-        }
-        return base.GetPropertyHeight(property, label);
-    }
-    void DrawHelpBox(Rect position, SerializedProperty prop)
-    {
-        // No need for a help box if the pattern is valid.
-        if (IsValid(prop))
-            return;
-
-        EditorGUI.HelpBox(position, string.Format("Invalid Range X [{0}]-[{1}] Y [{2}]-[{3}] Z [{4}]-[{5}]",
-            rangeAttribute.fMinX, rangeAttribute.fMaxX, rangeAttribute.fMinY, rangeAttribute.fMaxY, rangeAttribute.fMinZ, rangeAttribute.fMaxZ), MessageType.Error);
-    }
-    bool IsValid(SerializedProperty prop)
-    {
-        Vector3 vector = prop.vector3Value;
-        return vector.x >= rangeAttribute.fMinX && vector.x <= rangeAttribute.fMaxX
-            && vector.y >= rangeAttribute.fMinY && vector.y <= rangeAttribute.fMaxY
-            && vector.z >= rangeAttribute.fMinZ && vector.z <= rangeAttribute.fMaxZ;
     }
 }
